f32965883616479cbbdae66357af7d3cca02f36b
ChristopherRP <naruchristopher@gmail.com>
2020-07-13 15:59:21 -0600

    hola

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 
 /**
@@ -16,6 +12,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola");
     }
     
 }
